938595f0242b35c58812d6ba1c917d75850f2b10
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2016-11-08 14:10:37 +0300

    refactoring auth module for refresh token

--- test/auth.js
+++ test/auth.js
@@ -9,17 +9,34 @@ const Parasut = require('../');
 const id = process.env.clientId;
 const secret = process.env.clientSecret;
 const firmNo = process.env.firmNo;
+const email = process.env.email;
+const password = process.env.password;
 
-const parasut = new Parasut(id, secret, firmNo);
+const parasut = new Parasut(id, secret, firmNo, email, password);
 
 describe('Get AccessToken', () => {
   it('should work', (done) => {
     parasut.authorize((err, accessToken, refreshToken, results) => {
       expect(err).not.to.be.exist();
       expect(accessToken).to.be.exist();
-      expect(refreshToken).not.to.be.exist();
+      expect(refreshToken).to.be.exist();
       expect(results).to.be.exist();
       done();
+    });
+  });
+});
+
+describe('Get Refresh Token', () => {
+  it('should work', (done) => {
+    parasut.authorize((err) => {
+      expect(err).not.to.be.exist();
+      parasut.refreshToken((refreshError, accessToken, refreshToken, results) => {
+        expect(refreshError).not.to.be.exist();
+        expect(accessToken).to.be.exist();
+        expect(refreshToken).to.be.exist();
+        expect(results).to.be.exist();
+        done();
+      });
     });
   });
 });
--- test/contact.js
+++ test/contact.js
@@ -9,8 +9,10 @@ const Parasut = require('../');
 const id = process.env.clientId;
 const secret = process.env.clientSecret;
 const firmNo = process.env.firmNo;
+const email = process.env.email;
+const password = process.env.password;
 
-const parasut = new Parasut(id, secret, firmNo);
+const parasut = new Parasut(id, secret, firmNo, email, password);
 
 describe('Get Contacts', () => {
   before((done) => {
@@ -20,6 +22,8 @@ describe('Get Contacts', () => {
     parasut.contacts.list((error, body) => {
       expect(error).not.to.be.exist();
       expect(body).to.be.exist();
+      expect(body.items).to.be.exist();
+      expect(body.meta).to.be.exist();
       done();
     });
   });
